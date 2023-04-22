134735ed0f74a60c05cd6272ea6cfb528200f541
Camille Rubia <va.camillerubia@gmail.com>
2023-04-22 13:09:32 +0800

    Activity 2 Debugging complete

--- sandbox/debugging.js
+++ sandbox/debugging.js
@@ -1,9 +1,12 @@
 const PI = 3.14;
-let radius = 3;
 let area = 0;
-area = radius * radius * PI;
-console.log('First Area:', area);
 
-radius = 4;
-area = radius * radius * PI;
-console.log('Second Area:', area);
+function circleArea(radius)  {
+    return area = radius * PI;
+}
+
+area = circleArea(3);
+console.log("First area:", area);
+
+area = circleArea(4);
+console.log("Second area:", area);
