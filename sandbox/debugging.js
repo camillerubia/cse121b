84a0a34731121f7249851d0914a2af14837fe87e
Camille Rubia <va.camillerubia@gmail.com>
2023-04-22 12:58:01 +0800

    Activity 1 Debugging complete

---
+++ sandbox/debugging.js
@@ -0,0 +1,9 @@
+const PI = 3.14;
+let radius = 3;
+let area = 0;
+area = radius * radius * PI;
+console.log('First Area:', area);
+
+radius = 4;
+area = radius * radius * PI;
+console.log('Second Area:', area);
