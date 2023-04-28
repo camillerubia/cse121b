f1575a717778c77c8a8209d2d015ac84e39d9646
Camille Rubia <va.camillerubia@gmail.com>
2023-04-28 14:49:55 +0800

    Answer to W02 Activity 2 #4

--- w02/dom-basics.js
+++ w02/dom-basics.js
@@ -10,3 +10,13 @@ document.body.appendChild(image);
 const newDiv = document.createElement('div');
 newDiv.innerHTML = '<ul><li>One</li><li>Two</li><li>Three</li></ul>';
 document.body.appendChild(newDiv);
+
+const newSection = document.createElement('section');
+const newH2 = document.createElement('h2');
+const paragraphNew = document.createElement('p');
+newH2.innerText = 'CSE 121b';
+paragraphNew.innerText = 'Welcome to Javascript Language';
+document.body.appendChild(newSection);
+document.body.appendChild(newH2);
+document.body.appendChild(paragraphNew);
+
