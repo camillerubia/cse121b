dc6a5ccd7202279528d460cbb65819d7c2145585
Camille Rubia <va.camillerubia@gmail.com>
2023-05-05 08:44:09 +0800

    Finished Activity 2

--- week03/arrays.js
+++ week03/arrays.js
@@ -1,10 +1,26 @@
 // Activity 1
 
-const array = ['one', 'two', 'three'];
-const arrayHtml = array.map(function (step){
-    return `<li>${step}</li>`;
-});
+// const array = ['one', 'two', 'three'];
+// const arrayHtml = array.map(function (step){
+//     return `<li>${step}</li>`;
+// });
 
-document.getElementById("myList").innerHTML = arrayHtml.join(" ");
+// document.getElementById("myList").innerHTML = arrayHtml.join(" ");
 
 // Activity 2
+
+const grades = ['A', 'B', 'A'];
+
+function convertGrade(grade) {
+    let gpa = 0;
+
+    if (grade === 'A') {
+        gpa = 4;
+    }
+    else if (grade === 'B') {
+        gpa = 2;
+    }
+    return gpa;
+}
+// document.getElementById("myList").innerHTML = grades.map(convertGrade);
+const finalGpa = grades.map(convertGrade);
