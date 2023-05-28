7a0051aab114c28f1cc46d8ed84f26857a010968
Camille Rubia <va.camillerubia@gmail.com>
2023-05-28 09:34:02 +0800

    Added extra documentation

--- week06/scripts/task-project.js
+++ week06/scripts/task-project.js
@@ -1,12 +1,13 @@
-// Ensures the functions will run when the website has already loaded.
+// Ensures the functions will run after the HTML has already loaded.
 document.addEventListener("DOMContentLoaded", () => {
 
     // Created a list for subject, verb and objects
-    const subjects = ['My sandwich', 'Your pet', 'The plane', 'A duck', "The little girl"];
-    const verbs = ['ate', 'climbed over', 'slept on', 'ran towards', 'laughed'];
-    const objects = ['our fence', 'the park', 'the bowl', 'the beach', 'a comb'];
+    const subjects = ['My sandwich', 'Your pet', 'The plane', 'A duck', 'The little girl', 'A painting'];
+    const verbs = ['ate', 'climbed over', 'slept on', 'ran towards', 'laughed at', 'looked at'];
+    const objects = ['our fence', 'the park', 'the bowl', 'the beach', 'a comb', 'your picture'];
 
-    // store buttons and the container for displaying the words by subject, verb and object
+    // store buttons and the container in variables that displays the words 
+    // by subject, verb and object for local usage
     const dispButton = document.querySelector('.btn-generate');
     const dispSubject = document.querySelector('.subject-text');
     const dispVerb = document.querySelector('.verb-text');
@@ -29,6 +30,8 @@ document.addEventListener("DOMContentLoaded", () => {
         dispSubject.textContent = randomSubject;
         dispVerb.textContent = randomVerb;
         dispObject.textContent = randomObject;
+
+        // Calls the function for display effect/animation
         fadeIn();
     });
 
@@ -39,6 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
         verbs.push(inputVerb.value);
         objects.push(inputObject.value);
 
+        // Calls the function to display the words from input container at the bottom
         displayAddedWord();
 
         // Clears the input field when button is clicked.
@@ -48,29 +52,25 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Event listener for view button to display the list of words from array and 
-    // displaying it on the front end
+    // display it
     btnView.addEventListener('click', () => {
-        // resets the list so it won't show double list
+        // resets the list container so it won't repeat twice
         document.querySelector(".subject-list").innerHTML = "";
         document.querySelector(".verb-list").innerHTML = "";
         document.querySelector(".object-list").innerHTML = "";
 
-        // Goes through the array then creates an element to add the value one by 
-        // one and display
+        // Foreach loops in every category that goes through the array, creates an element 
+        // to add the value one by one and replace the HTML by category
         subjects.forEach(function(subject){
             var span = document.createElement('span');
             span.innerHTML = subject;
             document.querySelector(".subject-list").appendChild(span);
-        });
-        // Goes through the array then creates an element to add the value one by 
-        // one and display
+        })
         verbs.forEach(function(verb){
             var span = document.createElement('span');
             span.innerHTML = verb;
             document.querySelector(".verb-list").appendChild(span);
         });
-        // Goes through the array then creates an element to add the value one by 
-        // one and display
         objects.forEach(function(object){
             var span = document.createElement('span');
             span.innerHTML = object;
@@ -79,77 +79,81 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
 
-    // display the value of the input fields and displaying it on the front end
+    // display the value of the input fields and display it
     function displayAddedWord() {
         document.getElementById("addedWords").innerHTML = "";
+        
+        // Selects all input element from the input field container 
         const wordList = document.querySelectorAll('.input-fields-wrapper input');
+
+        // Loops through the list so each input will create a span and add value to
+        // append in the addedWords container
         wordList.forEach(function(word){
             var span = document.createElement("span");
             span.innerHTML = word.value + " ";
             document.getElementById("addedWords").appendChild(span);
-        });
-        
-        
+        }); 
     }
 
     // Functions to generate random words to create a sentence.
     function generateRandomSubject() {
         const randomSubject = subjects[Math.floor(Math.random() * subjects.length)];
-
         return randomSubject;
     }
     function generateRandomVerb() {
         const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-
         return randomVerb;
     }
     function generateRandomObject() {
         const randomObject = objects[Math.floor(Math.random() * objects.length)];
-
         return randomObject;
     }
 
     // This function will set the opacity of the generated words
     function fadeIn(){
+    
+        // resets the opacity to 0 as user clicks the Generate button
+        dispSubject.style.opacity = 0;
+        dispVerb.style.opacity = 0;
+        dispObject.style.opacity = 0;
+
         // Counters for opacity in each category.
         var op1 = 0;
         var op2 = 0;
         var op3 = 0;
 
-        // Avoids spamming the generate button
+        // Prevents the user from spamming the generate button
         dispButton.disabled = true;
 
         // Timers for displaying each word after another.
-        // but will stop if opacity has reached 1.
+        // but will stop if opacity counter has reached 1.
         var timer = setInterval(function () {
             if (op1 <= 1){
                 dispSubject.style.opacity = op1;
                 op1 += 0.1;
             }
             else{
+                var timer1 = setInterval(function () {
+                    if (op2 <= 1){
+                        dispVerb.style.opacity = op2;
+                        op2 += 0.1;
+                    }
+                    else{
+                        var timer2 = setInterval(function () {
+                            if (op3 <= 1){
+                                dispObject.style.opacity = op3;
+                                op3 += 0.1;
+                            }
+                            else{
+                                dispButton.disabled = false;
+                                clearInterval(timer2);
+                            }
+                        }, 100);
+                        clearInterval(timer1);
+                    }
+                }, 100);
                 clearInterval(timer);
             }
-        }, 50);
-
-        var timer1 = setInterval(function () {
-            if (op2 <= 1){
-                dispVerb.style.opacity = op2;
-                op2 += 0.1;
-            }
-            else{
-                clearInterval(timer1);
-            }
-        }, 200);
-
-        var timer2 = setInterval(function () {
-            if (op3 <= 1){
-                dispObject.style.opacity = op3;
-                op3 += 0.1;
-            }
-            else{
-                clearInterval(timer2);
-                dispButton.disabled = false;
-            }
-        }, 250);
+        }, 100);        
     }
 });
